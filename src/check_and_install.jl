a9e13b7a0359ac3592ca330696ab35b4a92ed328
Miklas Schütte <schuettem@irs.uni-stuttgart.de>
2024-10-30 12:01:55 +0100

    installing nrlmsise00

--- src/check_and_install.jl
+++ src/check_and_install.jl
@@ -6,7 +6,9 @@ function check_and_install_nrlmsise00()
     catch e
         @info "loading of nrlmsise00 failed. Installing nrlmsise00 via pip..."
         if isa(e, PyCall.PyError) && occursin("ModuleNotFoundError", e.msg)
+            @info "start installing nrlmsise00 via pip..."
             run(`$(PyCall.python) -m pip install nrlmsise00`)
+            @info "installing nrlmsise00 finished."
             return pyimport("nrlmsise00")
         else
             @info "installing of nrlmsise00 failed. Please install nrlmsise00 via pip."
